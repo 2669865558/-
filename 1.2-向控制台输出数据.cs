5400e63b81324b578f531977f25ef8060281a41b
门掩梨花棠未眠 <57624811+SNYJYZM@users.noreply.github.com>
2024-02-27 23:12:23 +0800

    Add files via upload

---
+++ 1.2-向控制台输出数据.cs
@@ -0,0 +1,19 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1 {
+    internal class Program {
+        static void Main(string[] args) {
+
+            Console.Write("输出不换行"); //输出字符串不换行
+            Console.WriteLine("输出后换行"); //输出后换行
+            Console.WriteLine("1234");
+            Console.WriteLine(DateTime.Now);
+            Console.ReadKey();
+
+        }
+    }
+}
